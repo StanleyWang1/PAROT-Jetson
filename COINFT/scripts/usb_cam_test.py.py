94e2eff9b7aa62101e0019423b21d224fbff8a47
vkojouharov <velinkojouharov@gmail.com>
2026-02-12 15:22:53 -0800

    camera

--- COINFT/scripts/usb_cam_test.py.py
+++ COINFT/scripts/usb_cam_test.py.py
@@ -11,8 +11,8 @@ while True:
     if not ret:
         print("Frame failed")
         break
-
-    cv2.imshow("Arducam USB", frame)
+    frame_small = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    cv2.imshow("camera", frame_small)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
